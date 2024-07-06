45da763e5f5ac0c93102313b92b7f3c9067dc6b0
Muhammad-Afzal-Bhatti <bhattiafzal095@gmail.com>
2024-07-06 18:30:25 +0500

    Write a program to check if given character is digit

--- main.dart
+++ main.dart
@@ -7,3 +7,14 @@ void main() {
   int num2 = int.parse(stdin.readLineSync()!);
   print("The sum is ${num1 + num2}");
 }
+
+
+// Write a program to check if given character is digit
+void main() {
+  String character = "6";
+  if (character.codeUnitAt(0) >= 48 && character.codeUnitAt(0) <= 57)
+    print("$character is a digit");
+  else {
+    print("$character is not a digit");
+  }
+}
