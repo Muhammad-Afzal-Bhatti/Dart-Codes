553484e82cfd716a509ea149fc39c9d6460b679a
Muhammad-Afzal-Bhatti <bhattiafzal095@gmail.com>
2024-07-11 11:14:21 +0500

    Write A Dart Program To Perform Binray Search

--- main.dart
+++ main.dart
@@ -18,3 +18,31 @@ void main() {
     print("$character is not a digit");
   }
 }
+
+
+// Write A Dart Program To Perform Binray Search
+import 'dart:io';
+void main() {
+  print("Enter a number that you want to search");
+  int name = int.parse(stdin.readLineSync()!);
+  List numbers = [2, 9, 13, 24, 29, 30, 35, 48, 50, 62, 67, 72, 84, 95];
+  int si = 0;
+  int ei = numbers.length - 1;
+  int mi = 0;
+  while (si <= ei) {
+    mi = (si + ei) ~/ 2;
+    if (name == numbers[mi]) {
+      print("Found at index $mi");
+      break;
+    } else if (name > numbers[mi]) {
+      si = mi + 1;
+    } else {
+      ei = mi - 1;
+    }
+    if (si > ei) {
+      print("Number Not Found");
+    }
+  }
+}
+
+
